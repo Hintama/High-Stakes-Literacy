c019a22ea2f3cb3226303059a5c5096f892924aa
Johnnyappseed <HaynesJ@asmsa.org>
2014-01-24 11:17:59 -0600

    needed, to commit?

--- 1st/src/Main.hx
+++ 1st/src/Main.hx
@@ -5,6 +5,9 @@ import flash.events.Event;
 import flash.Lib;
 import src.Ship;
 import flash.events.KeyboardEvent;
+import flash.display.Bitmap;
+import flash.display.BitmapData;
+import openfl.Assets;
 
 /**
  * ...
@@ -16,8 +19,9 @@ class Main extends Sprite
 	var ship:Ship;
 	var leftArrowDown:Bool;
 	var rightArrowDown:Bool;
-	var spaceBar:Bool;
 	var inited:Bool;
+	public static var game:Main;
+	public var bullets:List<Bullet>;
 
 	/* ENTRY POINT */
 	
@@ -45,7 +49,8 @@ class Main extends Sprite
 
 	public function new() 
 	{
-		super();	
+		super();
+		game = this;
 		addEventListener(Event.ADDED_TO_STAGE, added);
 		Lib.current.stage.addEventListener(KeyboardEvent.KEY_DOWN, keyDown);
 		Lib.current.stage.addEventListener(KeyboardEvent.KEY_UP, keyUp);
@@ -67,26 +72,24 @@ class Main extends Sprite
 	
 	function keyDown(e:KeyboardEvent)
 	{
-		if (e.keyCode == 32) spaceBar=true; 
 		if (e.keyCode == 37) leftArrowDown=true; 
 		if (e.keyCode == 39) rightArrowDown=true; 
 	}
 	
 	function keyUp(e:KeyboardEvent)
 	{
-		if (e.keyCode == 32) spaceBar=false; 
 		if (e.keyCode == 37) leftArrowDown=false; 
 		if (e.keyCode == 39) rightArrowDown=false; 
 	}
 
 	function atRefresh(e)
 	{
-		if (spaceBar) 
-		{
-			
-		}
-		if (leftArrowDown) ship.x -= 3;
-		if (rightArrowDown) ship.x += 3;
+		
+		if (leftArrowDown) ship.left();
+		if (rightArrowDown) ship.right();
+		ship.act();
+		for (bullet in bullets) bullet.act();
+		
 	}
 	public static function main() 
 	{
--- 1st/src/Ship.hx
+++ 1st/src/Ship.hx
@@ -2,7 +2,9 @@ package src;
 
 import flash.display.Bitmap;
 import flash.display.Sprite;
+import flash.display.BitmapData;
 import openfl.Assets;
+import flash.Lib;
 
 /**
  * ...
@@ -10,6 +12,7 @@ import openfl.Assets;
  */
 class Ship extends Sprite
 {
+	var v:Float;
 
 	public function new(x:Int, y:Int) 
 	{
@@ -18,7 +21,34 @@ class Ship extends Sprite
 		addChild(main_ship);
 		this.x = x;
 		this.y = y;
+		this.v = 0;
 	}
+	
+	public function left()
+	{
+		this.v -= 1;
+	}
+	
+	public function right()
+	{
+		this.v += 1;
+	}
+	
+        public function shoot()
+        {
+            var b:Bullet = new Bullet(Std.int(this.x + this.width / 2), Std.int(this.y), true);
+            Main.game.bullets.add(b);
+            Main.game.addChild(b);
+        }
+	
+	public function act()
+	{
+        if (this.x < 0 && this.v<0) this.v = 0;
+        if (this.x > 800 - this.width && this.v>0) this.v = 0;
+		v *= .95; 
+		this.x += this.v;
+	}
+	
 	
 	
 }
