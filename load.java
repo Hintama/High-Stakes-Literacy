0cb373732f36e277cc59db39919ffdf8eaad93fb
Johnnyappseed <HaynesJ@asmsa.org>
2014-03-06 19:02:05 -0600

    ghjjk

--- load.java
+++ load.java
@@ -7,14 +7,15 @@ public class load
 	public static PrintWriter fout;
 	public static void main(String args[])
 	{
-		String fileout="load3.txt";
+		String fileout="load1.txt";
 		openfiles(fileout);
 		int counter=0;
-		while (counter<25)
+		while (counter<125)
 		{
 			String word=getRandomWord();
-			if (word.length()<=4)
+			if (word.length()>7)
 			{
+				
 				counter++;
 				fout.write(word+",");
 				System.out.println(word);
